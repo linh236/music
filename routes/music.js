3e6b6ae44b76b82892251a33cd806f017c72e860
oLinhNguyen <nguyenquang.linh@access-company.com>
2022-05-21 09:08:33 +0700

    search songs

---
+++ routes/music.js
@@ -0,0 +1,62 @@
+var express = require("express");
+const { render } = require("express/lib/response");
+var router = express.Router();
+const fetch = require("node-fetch");
+
+/* GET users listing. */
+
+router.get("/", async function (req, res, next) {
+  fetch("http://mp3.zing.vn/xhr/recommend?type=audio")
+    .then((response) => response.json())
+    .then((json) => {
+      res.json(json.data["items"]);
+      // res.render("musics/index", { lists: json.data["items"] });
+    });
+});
+
+router.get("/show/:code", function (req, res, next) {
+  fetch(
+    `http://mp3.zing.vn/xhr/media/get-source?type=audio&key=${req.params.code}`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      res.render("musics/show", { song: json.data });
+    });
+});
+
+router.get("/info/:code", function (req, res, next) {
+  fetch(
+    `https://mp3.zing.vn/xhr/media/get-info?type=audio&id=${req.params.code}`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      res.render("musics/info", { info: json.data["info"] });
+    });
+});
+
+router.get("/play/:id", function (req, res, next) {
+  fetch(
+    `http://api.mp3.zing.vn/api/mobile/video/getvideoinfo?keycode=fafd463e2131914934b73310aa34a23f&requestdata={"id":"${req.params.id}"}`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.source !== undefined) {
+        // res.render("musics/play", { song: json.source });
+        res.json(json.source);
+      } else {
+        res.send("This music not video");
+      }
+    });
+});
+
+router.get("/search", function (req, res, next) {
+  let url = `http://ac.mp3.zing.vn/complete?type=artist,song,key,code&num=500&query=${req.query.search}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      res.json(json.data[0]['song']);
+      // res.render("musics/search", { lists: json.data[0]['song'], search: req.query.search });
+    });
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const fetch = require("node-fetch");
+const ytMusic = require("node-youtube-music");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async function (req, res, next) {
+  // res.send('respond with ');
+  fetch("http://mp3.zing.vn/xhr/chart-realtime?songId=0&videoId=0&albumId=0&chart=song&time=-1")
+    .then((response) => response.json())
+    .then(
+      (json) => {
+        res.render("users", { lists: json.data['song'] });
+      }
+    );
 });
 
 module.exports = router;
